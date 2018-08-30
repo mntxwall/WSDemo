072e97f38700cb2343ab8aa4cc00c9af9dcb7dc4
cw <userchenwei@gmail.com>
2018-08-30 11:27:10 +0800

    add bootstrap css

--- app/controllers/WSDemoController.scala
+++ app/controllers/WSDemoController.scala
@@ -2,6 +2,7 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 import play.api.Logger
+import play.api.libs.json.{JsPath, Reads}
 import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
 
@@ -15,20 +16,24 @@ class WSDemoController @Inject() (ws:WSClient, cc: ControllerComponents) (implic
 
     val complexRequest: WSRequest =
       ws.url("https://jsonplaceholder.typicode.com/todos/1")
+
     val futureResponse: Future[WSResponse] = complexRequest.get()
 
       futureResponse.map{
-        response =>
-        Logger.debug(response.json.toString)
+        response => {
+          Logger.debug("The title is " + response.json("title").toString())
+          //Logger.debug(response.json.toString)
+        }
 
     }
+
+   // val nameReads: Reads[String] = (JsPath \ "name").read[String]
 
 
     /*response =>
        (response.json \ "userId" \ "id" \ "title" \ "complete").as[String]*/
     //futureResponse.map(i => Logger.debug("hello " + i))
    // Logger.debug(futureResponse.toString)
-
 
     Ok(views.html.ws())
 
