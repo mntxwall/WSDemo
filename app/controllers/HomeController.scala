436bc8908ffe441dc5a98287b2530178ca0d948a
cw <userchenwei@gmail.com>
2018-09-06 00:35:41 +0800

    future map and flatmap

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,7 +5,12 @@ import models.UserTable
 import play.api._
 import play.api.data.Forms._
 import play.api.data.Form
+import play.api.libs.json.Json
 import play.api.mvc._
+
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration.DurationInt
+
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -14,7 +19,8 @@ import play.api.mvc._
 
 
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents)(implicit assetsFinder: AssetsFinder) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents)(implicit assetsFinder: AssetsFinder,
+                                                         ec: ExecutionContext) extends AbstractController(cc) {
 
   /**
    * Create an Action to render an HTML page.
@@ -26,7 +32,7 @@ class HomeController @Inject()(cc: ControllerComponents)(implicit assetsFinder: 
   def index() = Action { implicit request: Request[AnyContent] =>
 
 
-    Ok(views.html.index())
+    Ok(views.html.index(UserTable("","","")))
   }
 
 
@@ -39,6 +45,74 @@ class HomeController @Inject()(cc: ControllerComponents)(implicit assetsFinder: 
     Ok("hello")
   }
 
+  def hellojob(email: String, passwd: String, userDate: String) = Action{implicit request =>
+
+    Ok(views.html.index(UserTable(email, passwd, userDate)))
+  }
+  def addTwo(n:Int):Future[Int] = Future { n + 2 }
+
+  def addTwonoFuture(n: Int):Int = {n*2}
+
+  def addTwoAndDouble(n:Int):Future[Int] =
+    addTwo(n).map { x:Int =>
+      addTwonoFuture(x)
+    }
+
+  def addTwoAddTwo(num: Int, n:Int):Future[Int] = {
+    //Thread.sleep(3000)
+    addTwo(n).flatMap { n2 => addTwo(n2).map(_ + num) }
+  }
+
+
+
+
+  def test() = Action.async{ implicit request =>
+
+    //val a1 = addTwoAndDouble(4)
+
+    val aa1 = addTwoAndDouble(4)
+
+
+    aa1.flatMap{r1 =>
+
+
+      println("r1 is " + r1)
+      //Await.result(aa1, 3.seconds)
+      Thread.sleep(2000)
+      addTwoAddTwo(r1, 4).map{r2 =>
+
+          println("r2 is " + r2)
+
+      }
+    }
+
+/*
+    for{
+      a1 <- addTwoAndDouble(4) //12
+      a2 <- addTwoAddTwo(a1, 4) //18
+    }yield {
+
+      println("a1 is " + a1);
+      println("a2 is " + a2);
+    }
+    */
+    //val a2 = addTwoAddTwo(4)
+
+    val b: Future[Int] = Future{
+      12
+    }
+
+    //Await.result(a2, 2.seconds)
+
+    //a1.map(println)
+    //a2.foreach(println)
+
+
+    b.map(bb =>{
+      Ok(Json.obj("name" -> bb))
+    })
+
+  }
 
   def deal() = Action{ implicit request: Request[AnyContent] =>
 
