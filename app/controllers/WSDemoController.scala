dbdd6469846cf5caa0ff479ba482863f58631e4d
cw <userchenwei@gmail.com>
2018-09-06 15:42:13 +0800

    form test data update

--- app/controllers/WSDemoController.scala
+++ app/controllers/WSDemoController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 import play.api.Logger
-import play.api.libs.json.{JsPath, Reads}
+import play.api.libs.json.{JsPath, Json, Reads}
 import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
 
@@ -10,6 +10,7 @@ import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class WSDemoController @Inject() (ws:WSClient, cc: ControllerComponents) (implicit ec: ExecutionContext) extends AbstractController(cc) {
+
 
   def index() = Action { implicit request: Request[AnyContent] =>
 
@@ -38,6 +39,50 @@ class WSDemoController @Inject() (ws:WSClient, cc: ControllerComponents) (implic
     Ok(views.html.ws())
 
 
+  }
+
+  val mashapeApiKey = "FbkOCUKf8hMopHO5I8u2pGBUli2Dp14ZSMIjsnh0ky1T5tmpK1"
+
+  private def getAvailableDomains(phrase: String): Future[List[String]] = {
+    val domainSearchUrl = "https://domainsearch.p.mashape.com/index.php?name="
+    val futureDomains = ws.url(domainSearchUrl + phrase)
+      .withHttpHeaders("X-Mashape-Key" -> mashapeApiKey,
+        "Accept" -> "application/json").get
+    futureDomains map { domainNames =>
+      //val domains: Map[String, String] =
+        domainNames.json.as[List[String]]
+
+    }
+  }
+
+
+
+  private def getNounPhrases(text: String): Future[List[String]] = {
+    val textAnalysisUrl = "https://textanalysis.p.mashape.com/textblob-noun-phrase-extraction"
+    val futureKeyPhrases = ws.url(textAnalysisUrl)
+      .withHttpHeaders("X-Mashape-Key" -> mashapeApiKey,
+        "Content-Type" -> "application/x-www-form-urlencoded",
+        "Accept" -> "application/json")
+      .post(Map("text" -> Seq(text)) )
+    futureKeyPhrases map { keyPhrases =>
+      (keyPhrases.json \ "noun_phrases").as[List[String]].map(phrase => phrase.replace(" ", ""))
+    }
+  }
+
+  private def getAvailableDomains(phrases: List[String]): Future[List[String]] = {
+    val a: List[Future[List[String]]] = phrases.map(phrase => getAvailableDomains(phrase))
+    Future.sequence(a).map(_.flatten)
+    //Future.sequence(phrases.flatMap(phrase => getAvailableDomains(phrase)))
+  }
+
+
+  def getDomainNames(text: String) = Action.async { implicit request =>
+    for {
+      phrases <- getNounPhrases(text)
+      domains <- getAvailableDomains(phrases)
+    } yield {
+      Ok(Json.obj("available_domains" -> domains.mkString(";")))
+    }
   }
 
 
